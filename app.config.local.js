2e4082a85d230cf0d1049d61af71a91fcc0d1cb9
heverdumonde <imdosms08@naver.com>
2026-02-14 22:29:50 +0900

    Add files via upload

---
+++ app.config.local.js
@@ -0,0 +1,10 @@
+// Local runtime config (do not commit)
+window.__APP_CONFIG__ = {
+  GOOGLE_CLIENT_ID: "PASTE_YOUR_CLIENT_ID.apps.googleusercontent.com",
+  SUPABASE_URL: "https://fihlpblqxfymcauvvgmi.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_cDKf5EpUrZfq4u_iYulHlw_zBQ_0gip",
+  SCHOOL_DOMAIN: "onedu.jje.go.kr",
+  EMAIL_PREFIX: "ps2",
+  ADMIN_CODE: "2026",
+  ADMIN_EMAIL: "ps24029@onedu.jje.go.kr"
+};
